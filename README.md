ecde52c456aff02a5c3c5e34a1cc08738a24ef60
Benjamin Stein <ben@liatrio.com>
2018-05-10 11:30:53 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# pipeline-manager
+
+This repo manages pipelines for an organization.
+
+### Prerequisites
+* Bitbucket Organization Plugin
+* Projects setup in bitbucket
+
+## Why
+
+Pipelines can now be written as code, but something is needed to manage the pipeline references. This repo can be used to organize all your bitbucket projects in jenkins and set up all the shared-library references.
+
+## Usage
+1. Fork this repo into your organization.
+2. Edit the 
+
+
+## Power moves
+1. Used with https://github.com/liatrio/pipeline-developer, you can verify that everything is working as expected.
---
+++ pipelineGenerator.groovy
@@ -0,0 +1,36 @@
+import groovy.json.JsonSlurper
+
+
+def parsedJson = new JsonSlurper().parseText(readFileFromWorkspace('config.json'))
+// def gitRepos = parsedJson.gitRepos
+def gitLocation = parsedJson.bitbucketUrl
+def gitProjects = parsedJson.gitProjects
+gitProjects.each {
+    bitbucketProject->
+    organizationFolder(bitbucketProject.name) {
+        configure {
+            node ->
+            node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits' / 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
+                strategyId '1'
+            }
+            node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' {
+                serverUrl parsedJson.bitbucketUrl
+                credentialsId parsedJson.credentialId
+                repoOwner bitbucketProject.name
+            }
+            parsedJson.pipelineLibraries.each {
+                libraryUrl ->
+                def gitRepoName = (libraryUrl =~ /[a-z,-]*(?:\.git)/)[0] - '.git'
+                node / 'properties' / 'org.jenkinsci.plugins.workflow.libs.FolderLibraries' / 'libraries' << 'org.jenkinsci.plugins.workflow.libs.LibraryConfiguration' {
+                        retriever (class:'org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever') / scm (class:'jenkins.plugins.git.GitSCMSource') {
+                        remote libraryUrl
+                        credentialsId parsedJson.credentialId
+                    }
+                    defaultVersion 'master'
+                    implicit 'true'
+                    name gitRepoName
+                }
+            }
+        }
+    }
+}

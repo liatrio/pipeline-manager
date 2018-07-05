97a74f4a1f71b58c02085c0f019122cff65e7c34
Benjamin Stein <ben@liatrio.com>
2018-07-05 15:02:35 -0700

    setup configuration for folder level projects

--- pipelineGenerator.groovy
+++ pipelineGenerator.groovy
@@ -2,32 +2,43 @@ import groovy.json.JsonSlurper
 
 def parsedJson = new JsonSlurper().parseText(readFileFromWorkspace('config.json'))
 def gitLocation = parsedJson.bitbucketUrl
-def projectFolders = parsedJson.folders
-projectFolders.each {
-    folder(it.name)
-    bitbucketProject->
-    organizationFolder(it.name/bitbucketProject.name) {
-        configure {
-            node ->
-            node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits' / 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
-                strategyId '1'
-            }
-            node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' {
-                serverUrl parsedJson.bitbucketUrl
-                credentialsId parsedJson.credentialId
-                repoOwner bitbucketProject.name
-            }
-            parsedJson.pipelineLibraries.each {
-                libraryUrl ->
-                def gitRepoName = (libraryUrl =~ /[a-z,-]*(?:\.git)/)[0] - '.git'
-                node / 'properties' / 'org.jenkinsci.plugins.workflow.libs.FolderLibraries' / 'libraries' << 'org.jenkinsci.plugins.workflow.libs.LibraryConfiguration' {
-                        retriever (class:'org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever') / scm (class:'jenkins.plugins.git.GitSCMSource') {
-                        remote libraryUrl
-                        credentialsId parsedJson.credentialId
+def topLevelFolders = parsedJson.folders
+topLevelFolders.each {
+    topLevelFolder->
+    folder(topLevelFolder.name)
+    topLevelFolder.projects.each {
+        bitbucketProject->
+        def organizationInFolder = topLevelFolder.name + "/" + bitbucketProject
+        organizationFolder(organizationInFolder) {
+            configure {
+                node ->
+                node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' {
+                    serverUrl parsedJson.bitbucketUrl
+                    credentialsId parsedJson.credentialId
+                    repoOwner bitbucketProject
+                }
+                node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits' / 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
+                    strategyId '1'
+                }
+                node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits' / 'com.cloudbees.jenkins.plugins.bitbucket.OriginPullRequestDiscoveryTrait' {
+                    strategyId '1'
+                }
+                node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits' / 'com.cloudbees.jenkins.plugins.bitbucket.ForkPullRequestDiscoveryTrait' {
+                    strategyId '1'
+                    trust(class:'com.cloudbees.jenkins.plugins.bitbucket.ForkPullRequestDiscoveryTrait$TrustEveryone')
+                }
+                parsedJson.pipelineLibraries.each {
+                    libraryUrl ->
+                    def gitRepoName = (libraryUrl =~ /[a-z,-]*(?:\.git)/)[0] - '.git'
+                    node / 'properties' / 'org.jenkinsci.plugins.workflow.libs.FolderLibraries' / 'libraries' << 'org.jenkinsci.plugins.workflow.libs.LibraryConfiguration' {
+                            retriever (class:'org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever') / scm (class:'jenkins.plugins.git.GitSCMSource') {
+                            remote libraryUrl
+                            credentialsId parsedJson.credentialId
+                        }
+                        defaultVersion 'master'
+                        implicit 'true'
+                        name gitRepoName
                     }
-                    defaultVersion 'master'
-                    implicit 'true'
-                    name gitRepoName
                 }
             }
         }
